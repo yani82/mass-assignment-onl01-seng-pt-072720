fb8c0f17fa1bc233dfda26db7828c9c7a1df4f21
Yani Asari <yani.iman1@gmail.com>
2020-08-31 00:26:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,16 @@ class Person
   :height, :weight, :handed, :complexion, :t_shirt_size, 
   :wrist_size, :glove_size, :pant_length, :pant_width
 
-
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send(("#{key}="), value)
+    end
+  end 
 
 end
+
+def initialize(attributes)
+    attributes.each do |key, value|
+      self.send(("#{key}="), value)
+    end
+  end
